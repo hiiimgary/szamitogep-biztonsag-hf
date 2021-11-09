a435c3a18fdcb6749bc8d095a27d051fd4ea08e5
Gregurevic <paul.greg.indy@gmail.com>
2021-11-09 01:14:20 +0100

    added gif conversion library with usage sample code in main.cpp

--- CAFFParser/main.cpp
+++ CAFFParser/main.cpp
@@ -1,8 +1,21 @@
-#include "caff_parser.h"
-#include <iostream>
-#include <fstream>
+#include <vector>
+#include <cstdint>
+#include "converter/gif.h"
 
 int main(int argc, const char** argv)
 {
+    int width = 100;
+    int height = 200;
+    std::vector<uint8_t> black(width * height * 4, 0);
+    std::vector<uint8_t> white(width * height * 4, 255);
+
+    auto fileName = "bwgif.gif";
+    int delay = 100;
+    GifWriter g;
+    GifBegin(&g, fileName, width, height, delay);
+    GifWriteFrame(&g, black.data(), width, height, delay);
+    GifWriteFrame(&g, white.data(), width, height, delay);
+    GifEnd(&g);
+
     return 0;
 }
