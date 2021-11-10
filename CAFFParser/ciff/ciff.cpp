4126d7aaf8b6ab710da4d6d68d498ea5430c3882
Gergő Pattantyus <pattantyus.gerg@gmail.com>
2021-11-10 20:45:32 +0100

    feat: ciff parse methods

--- CAFFParser/ciff/ciff.cpp
+++ CAFFParser/ciff/ciff.cpp
@@ -31,3 +31,32 @@ void Ciff::saveCiffPartsToVariables(std::vector<char> animation) {
 
 
 }
+
+uint64_t Ciff::parseCaption(std::vector<char> in, uint64_t from) {
+    int size = in.size();
+    char* tmp = new char[size];
+
+    int i = 0;
+    for (i; i < size; i++) {
+        tmp[i - from] = in[i + from];
+        if (in[i] == '\n') {
+            break;
+        }
+    }
+
+    tmp[i + 1] = '\0';
+    header.setCaption(tmp);
+    printf("Caption: %s", tmp);
+
+    delete[] tmp;
+
+    return i;
+}
+
+void Ciff::parseTags(std::vector<char> in, uint64_t from, uint64_t to) {
+
+}
+
+void Ciff::parseContent(std::vector<char> in, uint64_t from, uint64_t to, uint64_t width, uint64_t height) {
+
+}
