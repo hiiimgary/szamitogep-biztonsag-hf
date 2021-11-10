bcf2aa635c8fad0a8181ed51cb310058f59da408
Gregurevic <paul.greg.indy@gmail.com>
2021-11-10 21:19:49 +0100

    changed pixel vector behaviour in caff to gif converter

--- CAFFParser/converter/caff_to_gif.cpp
+++ CAFFParser/converter/caff_to_gif.cpp
@@ -2,8 +2,8 @@
 // Created by paulg on 2021. 11. 09..
 //
 
-#include <cstring>
 #include <vector>
+#include <cstdint>
 #include "../caff/caff.h"
 #include "../ciff/ciff_content.h"
 #include "gif.h"
@@ -14,37 +14,33 @@ void fileWriter(Caff* caff, const char *output_file) {
 
     /// initialize gif with gif-h and first ciff content
     GifWriter g;
-    int first_width = animations[0].getCiff()->getHeader().getWidth();
-    int first_height = animations[0].getCiff()->getHeader().getHeight();
-    int first_duration = animations[0].getDuration();
+    uint64_t first_width = animations[0].getCiff()->getHeader().getWidth();
+    uint64_t first_height = animations[0].getCiff()->getHeader().getHeight();
+    uint64_t first_duration = animations[0].getDuration();
     GifBegin(&g, output_file, first_width, first_height, first_duration);
 
     /// for each ciff, extract width, height, duration and pixel vectors
-    for (int i = 0; i < animations.size(); i++) {
+    for (auto & animation : animations) {
 
         /// atomic data
-        int width = animations[i].getCiff()->getHeader().getWidth();
-        int height = animations[i].getCiff()->getHeader().getHeight();
-        int duration = animations[i].getDuration();
+        uint64_t width = animation.getCiff()->getHeader().getWidth();
+        uint64_t height = animation.getCiff()->getHeader().getHeight();
+        uint64_t duration = animation.getDuration();
 
-        /// pixel vectors
-        vector<RGB> rgb_vector = animations[i].getCiff()->getContent().getPixels();
-        vector<uint8_t> red(rgb_vector.size());
-        vector<uint8_t> green(rgb_vector.size());
-        vector<uint8_t> blue(rgb_vector.size());
-        vector<uint8_t> empty_vector(rgb_vector.size());
-        for (int j = 0; i < rgb_vector.size(); j++) {
-            red[j] = rgb_vector[j].R;
-            green[j] = rgb_vector[j].G;
-            blue[j] = rgb_vector[j].B;
-            empty_vector[j] = 0;
+        /// construct pixel vector
+        vector<vector<RGB>> rgb_vector = animation.getCiff()->getContent().getPixels();
+        vector<uint8_t> pixel(width * height * 4);
+        for (int h = 0; h < height; h++) {
+            for (int w = 0; w < width; w++) {
+                pixel.push_back((uint8_t)rgb_vector[h][w].R);
+                pixel.push_back((uint8_t)rgb_vector[h][w].G);
+                pixel.push_back((uint8_t)rgb_vector[h][w].B);
+                pixel.push_back((uint8_t)77);
+            }
         }
 
-        /// gif frame construction
-        GifWriteFrame(&g, red.data(), width, height, duration);
-        GifWriteFrame(&g, green.data(), width, height, duration);
-        GifWriteFrame(&g, blue.data(), width, height, duration);
-        GifWriteFrame(&g, empty_vector.data(), width, height, duration);
+        /// construct gif frame
+        GifWriteFrame(&g, pixel.data(), width, height, duration);
     }
     GifEnd(&g);
 
