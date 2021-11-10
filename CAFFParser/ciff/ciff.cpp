58ac8bb8e173d6d699257c180295031e1e70e499
tdrabos <drabostamas98@gmail.com>
2021-11-10 20:46:21 +0100

    ciff parse 2

--- CAFFParser/ciff/ciff.cpp
+++ CAFFParser/ciff/ciff.cpp
@@ -6,7 +6,6 @@
 #include "ciff_content.h"
 #include "ciff.h"
 
-#include <utility>
 #include "../data_manipulation/helper.h"
 
 CiffHeader Ciff::getHeader() {
@@ -23,11 +22,20 @@ void Ciff::saveCiffPartsToVariables(std::vector<char> animation) {
     header.setMagic(vectorToString(trim(animation, 0, 3)));
 
     // Setting header size
-
+    header.setHeaderSize(toInt(trim(animation, 4, 11)));
 
     // Setting content size
+    header.setContentSize(toInt(trim(animation, 12, 19)));
 
     // Setting width and height
+    header.setWidth(toInt(trim(animation, 20, 27)));
+    header.setHeight(toInt(trim(animation, 28, 35)));
+
+//    uint64_t index = parseCaption(animation, 36);
+//
+//    parseTags(animation, index + 1, header_size);
+//
+//    parseContent(animation, header_size, animation.size(), width, height);
 
 
 }
