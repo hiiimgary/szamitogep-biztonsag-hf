89c1dff2633d9c1c88ea50e3d838e45d5b93279a
rekad97 <reka.domjan@gmail.com>
2021-12-04 01:30:52 +0100

    fix route with authguard

--- Trumpery/Trumpery/ClientApp/src/app/app.routing.ts
+++ Trumpery/Trumpery/ClientApp/src/app/app.routing.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
-    // canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: 'profile',
